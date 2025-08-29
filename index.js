6b837f4a31e8322a6cdf817481c9c3076623457c
Jerry Tang <snaketzy@gmail.com>
2025-08-30 05:52:57 +0800

    feat:实现简单元素定位

--- index.js
+++ index.js
@@ -10,11 +10,33 @@ const main = async () => {
     width: 1366,
     height: 768
   });
-  const url = "https://testlogin.viphrm.com/";
+  const url = "https://premoss.viphrm.com/";
   await window.loadURL(url);
 
   const page = await pie.getPage(browser, window);
-  console.log(page.url());
+  // page.on("response", (response) => {
+  //   if(response.url().includes("premoss.viphrm.com") && [".png",".js"].indexOf(response.url()) < 0) {
+  //     response.json().then((res) => {
+  //       console.log(`${response.url()}.response.json`, res)
+  //   })}
+  // })
+  // console.log(page.url());
+  // await page.waitForSelector('a[target="_self"]');
+  // const link = await page.$('a[target="_self" and text()="更多应用"]');
+  // await link.click();
+  
+  await page.waitForSelector('a[target="_self"]');
+  const element = await page.$("div.flex-vertical");
+  
+  // 获取元素坐标和尺寸
+  const boundingBox = await element.boundingBox();
+
+   // 计算元素中心点坐标
+  const x = boundingBox.x + boundingBox.width / 2;
+  const y = boundingBox.y + boundingBox.height / 2;
+
+  await page.mouse.move(x, y, { steps: 20 });
+
 };
 
 main();
